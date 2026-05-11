a602b9816acf08acc57301ad982c5e11da0c2eab
fabianDvas <fabianmartinfigueroapolanco@gmail.com>
2026-05-11 15:57:35 -0400

    arreglo de error no iniciaba en el menú

--- SalonManager.UI/Program.cs
+++ SalonManager.UI/Program.cs
@@ -32,7 +32,7 @@ namespace SalonManager.UI
             var db = appScope.ServiceProvider.GetRequiredService<SalonDbContext>();
             db.Database.EnsureCreated();
 
-            Application.Run(new FormCita());
+            Application.Run(new Form1());
         }
     }
 }
