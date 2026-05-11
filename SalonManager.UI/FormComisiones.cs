f09654c769c190bd08047802c920b3e3d115ee4a
Yunio <yuniorfeliz0271999@gmail.com>
2026-05-11 13:26:51 -0400

    Agrega archivos completos de FormComisiones

---
+++ SalonManager.UI/FormComisiones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SalonManager.UI
+{
+    public partial class FormComisiones : Form
+    {
+        public FormComisiones()
+        {
+            InitializeComponent();
+        }
+    }
+}
